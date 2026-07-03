aa48cddba94f30a84095dc53837040844cfcb8fa
Richard Gieg <richgieg@users.noreply.github.com>
2026-07-03 10:50:22 -0700

    Add id field to cards

--- src/types/Card.ts
+++ src/types/Card.ts
@@ -1,24 +1,28 @@
 export type Card = NumberCard | DrawTwoCard | ReverseCard | SkipCard;
 
 type NumberCard = {
+  readonly id: string;
   readonly type: "number";
   readonly color: Color;
   readonly value: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 };
 
 type DrawTwoCard = {
+  readonly id: string;
   readonly type: "drawTwo";
   readonly color: Color;
   readonly value: 20;
 };
 
 type ReverseCard = {
+  readonly id: string;
   readonly type: "reverse";
   readonly color: Color;
   readonly value: 20;
 };
 
 type SkipCard = {
+  readonly id: string;
   readonly type: "skip";
   readonly color: Color;
   readonly value: 20;
