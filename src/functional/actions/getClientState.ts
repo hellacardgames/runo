400c2cc299356d44d1e7a1268a3d2fa3dc96d145
Richard Gieg <richgieg@users.noreply.github.com>
2026-08-04 16:15:20 -0700

    Add getClientState action

---
+++ src/functional/actions/getClientState.ts
@@ -0,0 +1,58 @@
+import type { ClientState } from "../types/ClientState.js";
+import type { Game } from "../types/Game.js";
+
+type GetClientStateResult =
+  | {
+      readonly success: true;
+      readonly state: ClientState;
+      readonly game: Game;
+    }
+  | {
+      readonly success: false;
+      readonly error: "playerNotFound";
+    };
+
+export function getClientState(
+  game: Game,
+  playerId: string,
+): GetClientStateResult {
+  const player = game.players.find((p) => p.id === playerId);
+  if (!player) {
+    return { success: false, error: "playerNotFound" };
+  }
+
+  const currentPlayer = game.players[game.currentPlayerIndex];
+  if (!currentPlayer) {
+    throw new Error("currentPlayer is undefined.");
+  }
+
+  const state: ClientState = {
+    status: game.status,
+    gameId: game.id,
+    playerId,
+    username: player.username,
+    players: game.players.map((p) => ({
+      username: p.username,
+      numCards: p.hand.length,
+      score: p.score,
+    })),
+    hand: player.hand,
+    lastDiscard: game.discardPile[game.discardPile.length - 1] ?? null,
+    currentPlayerUsername: currentPlayer.username,
+    isReversed: game.isReversed,
+    expiresAt: game.expiresAt,
+    // chatMessages: game.chatMessages,
+  };
+
+  game = {
+    ...game,
+    players: game.players.map((p) => {
+      if (p.id === player.id) {
+        return { ...p, events: [] };
+      }
+      return p;
+    }),
+  };
+
+  return { success: true, state, game };
+}
