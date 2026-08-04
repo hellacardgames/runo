8f50c4368b022e2d6e53f4df6d215ff3ca863bcf
Richard Gieg <richgieg@users.noreply.github.com>
2026-08-04 12:40:07 -0700

    Improve event tests for dealCardToPlayer

--- src/functional/lib/dealCardToPlayer.test.ts
+++ src/functional/lib/dealCardToPlayer.test.ts
@@ -57,7 +57,7 @@ test("deals card to player", () => {
   ]);
 });
 
-test("emits card dealt events", () => {
+test("emits cardDealtToPlayer event to all players", () => {
   const player1: Player = {
     id: "player-id-001",
     userId: "user-id-001",
@@ -93,15 +93,76 @@ test("emits card dealt events", () => {
 
   game = dealCardToPlayer(game, player2);
 
-  expect(game.players[0]?.events.slice(-1)).toMatchObject([
-    { type: "cardDealtToPlayer", username: "username-002" },
-  ]);
+  expect(game.players[0]?.events).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        type: "cardDealtToPlayer",
+        username: "username-002",
+      }),
+    ]),
+  );
 
-  expect(game.players[1]?.events.slice(-2)).toMatchObject([
-    {
-      type: "cardDealt",
-      card: { type: "number", value: 1, color: "red", id: "card-id-003" },
-    },
-    { type: "cardDealtToPlayer", username: "username-002" },
-  ]);
+  expect(game.players[1]?.events).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        type: "cardDealtToPlayer",
+        username: "username-002",
+      }),
+    ]),
+  );
+});
+
+test("emits cardDealt event to player only", () => {
+  const player1: Player = {
+    id: "player-id-001",
+    userId: "user-id-001",
+    username: "username-001",
+    events: [],
+    hand: [],
+    score: 0,
+  };
+  const player2: Player = {
+    id: "player-id-002",
+    userId: "user-id-002",
+    username: "username-002",
+    events: [],
+    hand: [],
+    score: 0,
+  };
+
+  let game: StartedGame = {
+    id: "game-id-001",
+    createdAt: Date.now(),
+    expiresAt: Date.now() + EXPIRY_EXTENSION_MS,
+    status: "started",
+    players: [player1, player2],
+    drawPile: [
+      { type: "number", value: 1, color: "red", id: "card-id-001" },
+      { type: "number", value: 1, color: "red", id: "card-id-002" },
+      { type: "number", value: 1, color: "red", id: "card-id-003" },
+    ],
+    discardPile: [],
+    currentPlayerIndex: 0,
+    isReversed: false,
+  };
+
+  game = dealCardToPlayer(game, player2);
+
+  expect(game.players[0]?.events).not.toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        type: "cardDealt",
+        card: { type: "number", value: 1, color: "red", id: "card-id-003" },
+      }),
+    ]),
+  );
+
+  expect(game.players[1]?.events).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        type: "cardDealt",
+        card: { type: "number", value: 1, color: "red", id: "card-id-003" },
+      }),
+    ]),
+  );
 });
