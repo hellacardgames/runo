c997a58f63dbc7dfe86efa6677e53cde417cad6e
Richard Gieg <richgieg@users.noreply.github.com>
2026-08-05 17:17:28 -0700

    Add addWildCardToDiscardPile

---
+++ src/functional/lib/addWildCardToDiscardPile.ts
@@ -0,0 +1,18 @@
+import { addItemToCollection } from "./addItemToCollection.js";
+import type { Color, WildCard } from "../types/Card.js";
+import type { StartedGame } from "../types/Game.js";
+
+export function addWildCardToDiscardPile(
+  game: StartedGame,
+  card: WildCard,
+  color: Color,
+): StartedGame {
+  return {
+    ...game,
+    discardPile: addItemToCollection(game.discardPile, {
+      type: "discardedWild",
+      card,
+      color,
+    }),
+  };
+}
