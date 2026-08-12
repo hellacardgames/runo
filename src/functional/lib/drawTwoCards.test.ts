252ff16e28ec893d25c4828d1c8d243f2f5a1a79
Richard Gieg <richgieg@users.noreply.github.com>
2026-08-12 09:20:03 -0700

    Add drawTwoCards

---
+++ src/functional/lib/drawTwoCards.test.ts
@@ -0,0 +1,158 @@
+import { expect, test } from "vitest";
+import { EXPIRY_EXTENSION_MS } from "../constants.js";
+import { drawTwoCards } from "./drawTwoCards.js";
+import type { StartedGame } from "../types/Game.js";
+
+test("takes two cards from draw pile and gives to player", () => {
+  let game: StartedGame = {
+    id: "game-id-001",
+    createdAt: Date.now(),
+    expiresAt: Date.now() + EXPIRY_EXTENSION_MS,
+    status: "started",
+    players: [
+      {
+        id: "player-id-001",
+        userId: "user-id-001",
+        username: "username-001",
+        events: [],
+        hand: [],
+        score: 0,
+      },
+      {
+        id: "player-id-002",
+        userId: "user-id-002",
+        username: "username-002",
+        events: [],
+        hand: [{ type: "reverse", color: "red", id: "card-id-004" }],
+        score: 0,
+      },
+    ],
+    drawPile: [
+      { type: "number", value: 5, color: "green", id: "card-id-001" },
+      { type: "number", value: 9, color: "blue", id: "card-id-002" },
+      { type: "number", value: 1, color: "yellow", id: "card-id-003" },
+    ],
+    discardPile: [],
+    currentPlayerIndex: 0,
+    isReversed: false,
+  };
+
+  game = drawTwoCards(game, "player-id-002");
+
+  expect(game.drawPile).toHaveLength(1);
+
+  expect(game.players[1]?.hand).toEqual([
+    { type: "reverse", color: "red", id: "card-id-004" },
+    { type: "number", value: 1, color: "yellow", id: "card-id-003" },
+    { type: "number", value: 9, color: "blue", id: "card-id-002" },
+  ]);
+
+  expect(game.players[0]?.hand).toHaveLength(0);
+});
+
+test("emits drewCard events to player", () => {
+  let game: StartedGame = {
+    id: "game-id-001",
+    createdAt: Date.now(),
+    expiresAt: Date.now() + EXPIRY_EXTENSION_MS,
+    status: "started",
+    players: [
+      {
+        id: "player-id-001",
+        userId: "user-id-001",
+        username: "username-001",
+        events: [],
+        hand: [],
+        score: 0,
+      },
+      {
+        id: "player-id-002",
+        userId: "user-id-002",
+        username: "username-002",
+        events: [],
+        hand: [{ type: "reverse", color: "red", id: "card-id-004" }],
+        score: 0,
+      },
+    ],
+    drawPile: [
+      { type: "number", value: 5, color: "green", id: "card-id-001" },
+      { type: "number", value: 9, color: "blue", id: "card-id-002" },
+      { type: "number", value: 1, color: "yellow", id: "card-id-003" },
+    ],
+    discardPile: [],
+    currentPlayerIndex: 0,
+    isReversed: false,
+  };
+
+  game = drawTwoCards(game, "player-id-002");
+
+  expect(game.players[1]?.events.slice(0, 2)).toEqual([
+    expect.objectContaining({
+      type: "drewCard",
+      card: { type: "number", value: 1, color: "yellow", id: "card-id-003" },
+    }),
+    expect.objectContaining({
+      type: "drewCard",
+      card: { type: "number", value: 9, color: "blue", id: "card-id-002" },
+    }),
+  ]);
+
+  expect(game.players[0]?.events).not.toEqual(
+    expect.arrayContaining([expect.objectContaining({ type: "drewCard" })]),
+  );
+});
+
+test("emits playerDrewTwoCards event to all players", () => {
+  let game: StartedGame = {
+    id: "game-id-001",
+    createdAt: Date.now(),
+    expiresAt: Date.now() + EXPIRY_EXTENSION_MS,
+    status: "started",
+    players: [
+      {
+        id: "player-id-001",
+        userId: "user-id-001",
+        username: "username-001",
+        events: [],
+        hand: [],
+        score: 0,
+      },
+      {
+        id: "player-id-002",
+        userId: "user-id-002",
+        username: "username-002",
+        events: [],
+        hand: [{ type: "reverse", color: "red", id: "card-id-004" }],
+        score: 0,
+      },
+    ],
+    drawPile: [
+      { type: "number", value: 5, color: "green", id: "card-id-001" },
+      { type: "number", value: 9, color: "blue", id: "card-id-002" },
+      { type: "number", value: 1, color: "yellow", id: "card-id-003" },
+    ],
+    discardPile: [],
+    currentPlayerIndex: 0,
+    isReversed: false,
+  };
+
+  game = drawTwoCards(game, "player-id-002");
+
+  expect(game.players[0]?.events).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        type: "playerDrewTwoCards",
+        username: "username-002",
+      }),
+    ]),
+  );
+
+  expect(game.players[1]?.events).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        type: "playerDrewTwoCards",
+        username: "username-002",
+      }),
+    ]),
+  );
+});
---
+++ src/functional/lib/drawTwoCards.ts
@@ -0,0 +1,26 @@
+import { takeCardsFromDrawPile } from "./takeCardsFromDrawPile.js";
+import { updatePlayer } from "./updatePlayer.js";
+import { addCardToHand } from "./addCardToHand.js";
+import { emitEventToPlayer } from "./emitEventToPlayer.js";
+import { emitEvent } from "./emitEvent.js";
+import { requirePlayer } from "./requirePlayer.js";
+import type { StartedGame } from "../types/Game.js";
+
+export function drawTwoCards(game: StartedGame, playerId: string): StartedGame {
+  const { player } = requirePlayer(game, playerId);
+
+  const takeCardsResult = takeCardsFromDrawPile(game, 2);
+  game = takeCardsResult.game;
+
+  takeCardsResult.cards.forEach((card) => {
+    game = updatePlayer(game, player.id, (p) => addCardToHand(p, card));
+    game = emitEventToPlayer(game, player.id, { type: "drewCard", card });
+  });
+
+  game = emitEvent(game, {
+    type: "playerDrewTwoCards",
+    username: player.username,
+  });
+
+  return game;
+}
