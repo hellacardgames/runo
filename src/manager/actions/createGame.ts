ed19a943687df36b2275ef9d7b5e0e02d8b191f9
Richard Gieg <richgieg@users.noreply.github.com>
2026-08-12 14:45:49 -0700

    Add return type for createGame

--- src/manager/actions/createGame.ts
+++ src/manager/actions/createGame.ts
@@ -1,7 +1,23 @@
 import { createGame as doCreateGame } from "../../actions/createGame.js";
+import { MAX_GAMES } from "../constants.js";
 import { games } from "../games.js";
 
-export function createGame(userId: string, username: string) {
+type CreateGameResult =
+  | {
+      readonly success: true;
+      readonly gameId: string;
+      readonly playerId: string;
+    }
+  | {
+      readonly success: false;
+      readonly error: "maxGamesReached";
+    };
+
+export function createGame(userId: string, username: string): CreateGameResult {
+  if (games.size === MAX_GAMES) {
+    return { success: false, error: "maxGamesReached" };
+  }
+
   const result = doCreateGame(userId, username);
 
   games.set(result.game.id, result.game);
