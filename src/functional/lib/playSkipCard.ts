2a63ef5148dc3dbf86d73da0ffa389dc01c0ce83
Richard Gieg <richgieg@users.noreply.github.com>
2026-08-11 18:02:29 -0700

    Implement playSkipCard

--- src/functional/lib/playSkipCard.ts
+++ src/functional/lib/playSkipCard.ts
@@ -1,3 +1,16 @@
+import { updatePlayer } from "./updatePlayer.js";
+import { removeCardFromHand } from "./removeCardFromHand.js";
+import { addCardToDiscardPile } from "./addCardToDiscardPile.js";
+import { emitEvent } from "./emitEvent.js";
+import { isOutOfCards } from "./isOutOfCards.js";
+import { isGameWinner } from "./isGameWinner.js";
+import { transitionGameToCompleted } from "./transitionGameToCompleted.js";
+import { changeTurn } from "./changeTurn.js";
+import { startRound } from "./startRound.js";
+import { returnDiscardPile } from "./returnDiscardPile.js";
+import { calculatePoints } from "./calculatePoints.js";
+import { raiseScore } from "./raiseScore.js";
+import { returnPlayerCards } from "./returnPlayerCards.js";
 import { getCurrentPlayer } from "./getCurrentPlayer.js";
 import type { SkipCard } from "../types/Card.js";
 import type { CompletedGame, StartedGame } from "../types/Game.js";
@@ -6,9 +19,49 @@ export function playSkipCard(
   game: StartedGame,
   card: SkipCard,
 ): StartedGame | CompletedGame {
-  const player = getCurrentPlayer(game);
+  const currentPlayer = getCurrentPlayer(game);
 
-  console.log(card, player);
+  game = updatePlayer(game, currentPlayer.id, (p) =>
+    removeCardFromHand(p, card),
+  );
+  game = addCardToDiscardPile(game, card);
+  game = emitEvent(game, {
+    type: "cardPlayed",
+    username: currentPlayer.username,
+    card,
+  });
+
+  if (isOutOfCards(game, currentPlayer.id)) {
+    game = raiseScore(game, currentPlayer.id, calculatePoints(game));
+    game = returnPlayerCards(game);
+    game = returnDiscardPile(game);
+
+    const updatedCurrentPlayer = getCurrentPlayer(game);
+
+    if (isGameWinner(game, currentPlayer.id)) {
+      game = emitEvent(game, {
+        type: "playerWonGame",
+        username: updatedCurrentPlayer.username,
+        score: updatedCurrentPlayer.score,
+      });
+      game = emitEvent(game, { type: "gameCompleted" });
+      return transitionGameToCompleted(game);
+    } else {
+      game = emitEvent(game, {
+        type: "playerWonRound",
+        username: updatedCurrentPlayer.username,
+        score: updatedCurrentPlayer.score,
+      });
+      game = startRound(game);
+    }
+  } else {
+    game = changeTurn(game);
+    game = changeTurn(game);
+    game = emitEvent(game, {
+      type: "turnChanged",
+      currentPlayerUsername: getCurrentPlayer(game).username,
+    });
+  }
 
   return game;
 }
