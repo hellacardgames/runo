364d67ab40a02aebff10e535627846bda5193394
Richard Gieg <richgieg@users.noreply.github.com>
2026-08-12 15:28:25 -0700

    Add getJoinableGames

---
+++ src/manager/actions/getJoinableGames.ts
@@ -0,0 +1,20 @@
+import { MAX_PLAYERS } from "../../constants.js";
+import { games } from "../games.js";
+
+type GetJoinableGamesResult = {
+  readonly games: readonly {
+    readonly id: string;
+    readonly numPlayers: number;
+  }[];
+};
+
+export function getJoinableGames(): GetJoinableGamesResult {
+  return {
+    games: Array.from(games.values())
+      .filter((g) => g.status === "created" && g.players.length < MAX_PLAYERS)
+      .map((g) => ({
+        id: g.id,
+        numPlayers: g.players.length,
+      })),
+  };
+}
