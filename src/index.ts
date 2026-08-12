a8b52d5e09b79f40742e9aeb7ad5c993746ee7be
Richard Gieg <richgieg@users.noreply.github.com>
2026-08-12 13:18:09 -0700

    Export acknowledgeAndGetEvents

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
+export { acknowledgeAndGetEvents } from "./actions/acknowledgeAndGetEvents.js";
 export { createGame } from "./actions/createGame.js";
 export { drawCard } from "./actions/drawCard.js";
 export { getClientState } from "./actions/getClientState.js";
-// export { getEvents } from "./actions/getEvents.js";
-// export { getJoinableGames } from "./actions/getJoinableGames.js";
 export { joinGame } from "./actions/joinGame.js";
 export { leaveGame } from "./actions/leaveGame.js";
 export { playCard } from "./actions/playCard.js";
