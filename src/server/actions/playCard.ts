446d8833d43c78c3ce735336add6af14c717a623
Richard Gieg <richgieg@users.noreply.github.com>
2026-08-14 11:37:36 -0700

    Add server

---
+++ src/server/actions/playCard.ts
@@ -0,0 +1,37 @@
+import { z } from "zod";
+import { playCard as doPlayCard } from "../../manager/actions/playCard.js";
+
+type PlayCardResult =
+  | {
+      readonly success: true;
+    }
+  | {
+      readonly success: false;
+      readonly error:
+        | "invalidInput"
+        | "gameNotFound"
+        | "invalidStatus"
+        | "playerNotFound"
+        | "outOfTurn"
+        | "cardNotFound"
+        | "cardIsWild"
+        | "cardNotPlayable";
+    };
+
+const inputSchema = z
+  .object({
+    gameId: z.string(),
+    playerId: z.string(),
+    cardId: z.string(),
+  })
+  .transform(
+    ({ gameId, playerId, cardId }) => [gameId, playerId, cardId] as const,
+  );
+
+export function playCard(input: unknown): PlayCardResult {
+  const parseResult = inputSchema.safeParse(input);
+  if (!parseResult.success) {
+    return { success: false, error: "invalidInput" };
+  }
+  return doPlayCard(...parseResult.data);
+}
