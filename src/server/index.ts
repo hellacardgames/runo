361fb65f7c8b59d41e0549897d405bd8fa4c0592
Richard Gieg <richgieg@users.noreply.github.com>
2026-08-16 09:55:48 -0700

    Export action list from server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,28 @@
-export { createGame } from "./actions/createGame.js";
-export { drawCard } from "./actions/drawCard.js";
-export { getClientStateAndClearEvents } from "./actions/getClientStateAndClearEvents.js";
-export { getEventsAndClearAcknowledged } from "./actions/getEventsAndClearAcknowledged.js";
-export { getJoinableGames } from "./actions/getJoinableGames.js";
-export { joinGame } from "./actions/joinGame.js";
-export { leaveGame } from "./actions/leaveGame.js";
-export { playCard } from "./actions/playCard.js";
-export { playWildCard } from "./actions/playWildCard.js";
-export { sendChat } from "./actions/sendChat.js";
-export { startGame } from "./actions/startGame.js";
+import { createGame } from "./actions/createGame.js";
+import { drawCard } from "./actions/drawCard.js";
+import { getClientStateAndClearEvents } from "./actions/getClientStateAndClearEvents.js";
+import { getEventsAndClearAcknowledged } from "./actions/getEventsAndClearAcknowledged.js";
+import { getJoinableGames } from "./actions/getJoinableGames.js";
+import { joinGame } from "./actions/joinGame.js";
+import { leaveGame } from "./actions/leaveGame.js";
+import { playCard } from "./actions/playCard.js";
+import { playWildCard } from "./actions/playWildCard.js";
+import { sendChat } from "./actions/sendChat.js";
+import { startGame } from "./actions/startGame.js";
+
+export const actions = [
+  { path: "/createGame", func: createGame },
+  { path: "/drawCard", func: drawCard },
+  { path: "/getClientStateAndClearEvents", func: getClientStateAndClearEvents },
+  {
+    path: "/getEventsAndClearAcknowledged",
+    func: getEventsAndClearAcknowledged,
+  },
+  { path: "/getJoinableGames", func: getJoinableGames },
+  { path: "/joinGame", func: joinGame },
+  { path: "/leaveGame", func: leaveGame },
+  { path: "/playCard", func: playCard },
+  { path: "/playWildCard", func: playWildCard },
+  { path: "/sendChat", func: sendChat },
+  { path: "/startGame", func: startGame },
+] as const;
