5fbf944b80ba5f317dd6b53e82c4b2c9a17c551f
Richard Gieg <richgieg@users.noreply.github.com>
2026-08-25 14:46:37 -0700

    Update lib and createManager

--- src/manager/createManager.ts
+++ src/manager/createManager.ts
@@ -12,7 +12,6 @@ import {
   sendChat,
   startGame,
 } from "../game/index.js";
-import type { Color } from "../game/index.js";
 
 export const createManager = createManagerFactory({
   maxPlayers: MAX_PLAYERS,
@@ -23,47 +22,9 @@ export const createManager = createManagerFactory({
   leaveGame,
   sendChat,
   startGame,
-  createCustomActions: (games) => ({
-    drawCard: (gameId: string, playerId: string) => {
-      const game = games.get(gameId);
-      if (!game) {
-        return { success: false, error: "gameNotFound" } as const;
-      }
-      const result = drawCard(game, playerId);
-      if (!result.success) {
-        return { success: false as const, error: result.error } as const;
-      }
-      games.set(gameId, result.game);
-      return { success: true } as const;
-    },
-    playCard: (gameId: string, playerId: string, cardId: string) => {
-      const game = games.get(gameId);
-      if (!game) {
-        return { success: false, error: "gameNotFound" } as const;
-      }
-      const result = playCard(game, playerId, cardId);
-      if (!result.success) {
-        return { success: false as const, error: result.error } as const;
-      }
-      games.set(gameId, result.game);
-      return { success: true } as const;
-    },
-    playWildCard: (
-      gameId: string,
-      playerId: string,
-      cardId: string,
-      color: Color,
-    ) => {
-      const game = games.get(gameId);
-      if (!game) {
-        return { success: false, error: "gameNotFound" } as const;
-      }
-      const result = playWildCard(game, playerId, cardId, color);
-      if (!result.success) {
-        return { success: false as const, error: result.error } as const;
-      }
-      games.set(gameId, result.game);
-      return { success: true } as const;
-    },
+  addCustomActions: (wrapAction) => ({
+    drawCard: wrapAction(drawCard),
+    playCard: wrapAction(playCard),
+    playWildCard: wrapAction(playWildCard),
   }),
 });
