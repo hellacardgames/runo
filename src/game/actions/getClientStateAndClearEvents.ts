05d1edd711b12cf27990519b989ae0a0af53f736
Richard Gieg <richgieg@users.noreply.github.com>
2026-08-25 10:27:24 -0700

    Update lib and use getClientStateAndClearEventsFactory

--- src/game/actions/getClientStateAndClearEvents.ts
+++ src/game/actions/getClientStateAndClearEvents.ts
@@ -1,32 +1,24 @@
-import { getCurrentPlayer, updatePlayer } from "@hellacardgames/lib";
+import { getClientStateAndClearEventsFactory } from "@hellacardgames/lib";
 import type { ClientState } from "../types/ClientState.js";
 import type { Game } from "../types/Game.js";
 
-export function getClientStateAndClearEvents(game: Game, playerId: string) {
-  const player = game.players.find((p) => p.id === playerId);
-  if (!player) {
-    return { success: false, error: "playerNotFound" } as const;
-  }
-
-  const state: ClientState = {
-    status: game.status,
-    gameId: game.id,
-    playerId,
-    username: player.username,
-    players: game.players.map((p) => ({
-      username: p.username,
-      numCards: p.hand.length,
-      score: p.score,
-    })),
-    hand: player.hand,
-    lastDiscard: game.discardPile[game.discardPile.length - 1] ?? null,
-    currentPlayerUsername: getCurrentPlayer(game).username,
-    isReversed: game.isReversed,
-    expiresAt: game.expiresAt,
-    chatMessages: game.chatMessages,
-  };
-
-  game = updatePlayer(game, player.id, (p) => ({ ...p, events: [] }));
-
-  return { success: true, state, game } as const;
-}
+export const getClientStateAndClearEvents = getClientStateAndClearEventsFactory<
+  Game,
+  ClientState
+>((game, player, currentPlayer) => ({
+  status: game.status,
+  gameId: game.id,
+  playerId: player.id,
+  username: player.username,
+  players: game.players.map((p) => ({
+    username: p.username,
+    numCards: p.hand.length,
+    score: p.score,
+  })),
+  hand: player.hand,
+  lastDiscard: game.discardPile[game.discardPile.length - 1] ?? null,
+  currentPlayerUsername: currentPlayer.username,
+  isReversed: game.isReversed,
+  expiresAt: game.expiresAt,
+  chatMessages: game.chatMessages,
+}));
