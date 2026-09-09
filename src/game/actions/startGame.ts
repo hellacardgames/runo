1ef52b1d8bf6590aaaf1babba49a7d5544edf1dd
Richard Gieg <richgieg@users.noreply.github.com>
2026-09-09 07:28:14 -0700

    Refactor startGame

--- src/game/actions/startGame.ts
+++ src/game/actions/startGame.ts
@@ -19,17 +19,16 @@ export function startGame(game: Game, playerId: string) {
     return { success: false, error: "minPlayersNotReached" } as const;
   }
 
-  let startedGame = transitionGameToStarted(game);
+  game = transitionGameToStarted(game);
+  game = emitEvent(game, { type: "gameStarted" });
 
-  startedGame = { ...startedGame, expiresAt: Date.now() + EXPIRY_EXTENSION_MS };
+  game = startRound(game);
 
-  startedGame = startRound(startedGame);
-
-  startedGame = emitEvent(startedGame, { type: "gameStarted" });
-  startedGame = emitEvent(startedGame, {
+  game = { ...game, expiresAt: Date.now() + EXPIRY_EXTENSION_MS };
+  game = emitEvent(game, {
     type: "expirationUpdated",
-    expiresAt: startedGame.expiresAt,
+    expiresAt: game.expiresAt,
   });
 
-  return { success: true, game: startedGame } as const;
+  return { success: true, game } as const;
 }
